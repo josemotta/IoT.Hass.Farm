ca1d54215c7b1d97c2246fbdc26c6ecc370aca97
Jose Motta <josemotta@bampli.com>
2019-08-15 18:30:06 -0300

    add adc component

--- _pump/bin/custom_components/ads1x15/sensor.py
+++ _pump/bin/custom_components/ads1x15/sensor.py
@@ -1,42 +1,94 @@
-"""ADS1x15 analog to digital converters."""
+"""ADS1x15 - Analog to Digital Converter."""
+
+import asyncio
+import logging
+from functools import partial
+
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components import rpi_gpio
+from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
-import time
 
-GAIN = 1
-ADC_INPUT = 0
+_LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the sensor platform."""
-    add_devices([ads_o2()])
+POWER_VOLT = "V"
+
+SENSOR_CH0 = "0"
+SENSOR_CH1 = "1"
+
+CONF_I2C_ADDRESS = "i2c_address"
+CONF_I2C_BUS = "i2c_bus"
+
+DEFAULT_NAME = "ADS1X15"
+DEFAULT_I2C_ADDRESS = 0x48
+DEFAULT_I2C_BUS = 1
+DEFAULT_GAIN = 1
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+        vol.Optional(CONF_I2C_ADDRESS, default=DEFAULT_I2C_ADDRESS): vol.Coerce(int),
+        vol.Optional(CONF_I2C_BUS, default=DEFAULT_I2C_BUS): vol.Coerce(int),
+    }
+)
 
 
-class ADS1x15(Entity):
-    """Representation of a Sensor."""
+# def init_bmp(bus_number, i2c_address, sensor):
+#     """XSHUT port LOW resets the device."""
+#     sensor(bus_number, i2c_address)
 
-    def __init__(self):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Reset and initialize the ADS1x15 Analog to Digital Converter."""
+
+    import smbus  # pylint: disable=import-error
+    import Adafruit_ADS1x15 as ADS  # pylint: disable=import-error
+
+    name = config.get(CONF_NAME)
+    bus_number = config.get(CONF_I2C_BUS)
+    i2c_address = config.get(CONF_I2C_ADDRESS)
+
+    sensor = await hass.async_add_executor_job(partial(ADS.ADS1015, i2c_address, bus_number))
+    # await hass.async_add_executor_job(init_bmp, bus_number, i2c_address, sensor)
+    # await asyncio.sleep(0.01)
+
+    dev = [
+        ADS1X15Sensor(sensor, name, SENSOR_CH0),
+        ADS1X15Sensor(sensor, name, SENSOR_CH1),
+    ]
+
+    async_add_entities(dev, True)
+
+
+class ADS1X15Sensor(Entity):
+    """Implementation of ADS1X15 sensor."""
+
+    def __init__(self, ads1x15_sensor, name, channel):
         """Initialize the sensor."""
+        self.ads1x15_sensor = ads1x15_sensor
+        self._name = "{}_{}".format(name, channel)
+        self._channel = channel
+        self._unit_of_measurement = POWER_VOLT
         self._state = None
-        # Create an ADS1115 ADC (16-bit) instance.
-        self._adc = Adafruit_ADS1x15.ADS1115()
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor."""
-        return 'O2 Level'
+        return self._name
 
     @property
-    def state(self):
+    def state(self) -> int:
         """Return the state of the sensor."""
         return self._state
 
     @property
-    def unit_of_measurement(self):
+    def unit_of_measurement(self) -> str:
         """Return the unit of measurement."""
-        return "%"
+        return self._unit_of_measurement
 
     def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._state = self._adc.read_adc(ADC_INPUT, gain=GAIN)
+        """Get the latest measurement and update state."""
+        value = self.ads1x15_sensor.read_adc(self._channel, DEFAULT_GAIN)
+        self._state = value
